6a5ac819913e01f93f898f0960d7ba6a2e4ca71e
venky7031 <venkyvenky7031@gmail.com>
2026-01-22 01:05:11 +0530

    terraform

---
+++ state/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "roboshop" {
+  ami           = var.aws_ami_id
+  instance_type = var.aws_instance_type
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+  tags = var.tag_name
+}
+
+
+resource "aws_security_group" "allow_all" {
+  name        = var.sg_name
+  description = var.description_name
+  
+ingress {
+    from_port        = var.from_port
+    to_port          = var.To_port
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+egress {
+    from_port        = var.from_port
+    to_port          = var.To_port
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = var.sg_tags
+}
---
+++ state/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "5.98.0"
+      }
+    
+}
+
+backend "s3" {
+    bucket       = "uppalavenky-84s-remote-state"
+    key          = "remote-state-demo"
+    use_lockfile = true
+    encrypt = true
+    region       = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+  
+}
