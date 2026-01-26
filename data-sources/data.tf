fe4a4ebb59b365d3ca88d194beecc21fdb357fcf
venky7031 <venkyvenky7031@gmail.com>
2026-01-26 19:53:00 +0530

    terraform

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -5,8 +5,18 @@ data "aws_ami" "joindevops" {
       name = "name"
       values = ["Redhat-9-DevOps-Practice"]
     }
+
+
+filter {
+  name   = "root-device-type"
+  values = ["ebs"]
 }
 
+filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+}
+}
 output "ami_id" {
   value = data.aws_ami.joindevops.id
 }
